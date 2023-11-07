94586c2a73f94da3a6ff385fc027f7e540f9d74a
Wabuuuu <122350091+Wabuuuu@users.noreply.github.com>
2023-11-07 13:31:23 +0100

    klar med foto app

--- docs/_app/immutable/chunks/9-c6863a7c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/fotoapp/_page.svelte-2317125e.js";export{t as component};
--- src/routes/fotoapp/+page.svelte
+++ src/routes/fotoapp/+page.svelte
@@ -1,18 +1,18 @@
 <title>Pontiff sulyvahn</title> /*"Greetings, welcome to lordran, where ageless grace and wonder unite.*/
-<h1>Pontiff sulyvahn</h1>
+<h1>Boss images</h1>
 
 <script> 
 let pontiff ="https://pbs.twimg.com/media/DsZoQchWkAACpcf?format=jpg&name=large"
 let artorias = "https://64.media.tumblr.com/17ad0d7a641de4a718156c5d60966047/tumblr_ofzbbspdgw1rggnpno9_640.jpg"
 let yhorm = "https://i.pinimg.com/236x/d4/e9/c4/d4e9c4193f808e01d5c7a50f2a3bc2fe.jpg"
-let images = [pontiff,artorias,yhorm]
-    
+let images = [pontiff,artorias,yhorm];
+   
 </script>
 
 <div class = "fotoapp">
-    <img src= {pontiff} alt="flexbox rules">
-    <img src= {artorias} alt="flexbox rules">
-    <img src= {yhorm} alt="flexbox rules">
+    <img src= {images[0]} alt="flexbox rules">
+    <img src= {images[1]} alt="flexbox rules">
+    <img src= {images[2]} alt="flexbox rules">
     
    
 
